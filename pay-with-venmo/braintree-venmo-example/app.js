ba6c39cf996521f598dddf057c9268ce62cb2a71
Travell Williams <58542411+travellw@users.noreply.github.com>
2022-03-22 10:37:24 -0500

    Add files via upload

---
+++ pay-with-venmo/braintree-venmo-example/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { join } = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan-debug');
+const { json, urlencoded } = require('body-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const router = require('./routes');
+const createError = require('http-errors');
+
+const app = express();
+const staticRoot = join(__dirname, 'public');
+
+// view engine setup
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(favicon(join(staticRoot, 'images', 'favicon.png')));
+app.use(logger('braintree_example:app', 'dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: '---',
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+app.use(express.static(staticRoot));
+app.use(flash());
+
+app.use('/', router);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  // eslint-disable-next-line no-unused-vars
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
+
+module.exports = app;
---
+++ pay-with-venmo/braintree-venmo-example/lib/gateway.js
@@ -0,0 +1,30 @@
+const braintree = require('braintree');
+const dotenv = require('dotenv');
+
+let localEnvironment, gateway;
+
+dotenv.config();
+
+if (
+  !process.env.BT_ENVIRONMENT ||
+  !process.env.BT_MERCHANT_ID ||
+  !process.env.BT_PUBLIC_KEY ||
+  !process.env.BT_PRIVATE_KEY
+) {
+  throw new Error(
+    'Cannot find necessary environment variables. See https://github.com/braintree/braintree_express_example#setup-instructions for instructions'
+  );
+}
+
+localEnvironment = `${process.env.BT_ENVIRONMENT.charAt(
+  0
+).toUpperCase()}${process.env.BT_ENVIRONMENT.slice(1)}`;
+
+gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment[localEnvironment],
+  merchantId: process.env.BT_MERCHANT_ID,
+  publicKey: process.env.BT_PUBLIC_KEY,
+  privateKey: process.env.BT_PRIVATE_KEY,
+});
+
+module.exports = gateway;
